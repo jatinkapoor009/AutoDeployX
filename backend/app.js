7dec4f41012656eb741c637cc2097d41b05f6b49
Ubuntu <ubuntu@ip-172-31-25-18.ec2.internal>
2026-04-20 09:26:55 +0000

    initial commit - Autodeployx

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.post('/api', (req, res) => {
+const name = req.body.name;
+
+res.json({
+message: `Hello ${name}, AutoDeployX backend working 🚀`
+});
+});
+
+app.listen(3000, () => {
+console.log("Backend running on port 3000");
+});
+
